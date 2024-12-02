eb38f88b6f59b0abbbf65badce8b8af1ea15eb3f
Din19062004 <haivietb9@gmail.com>
2024-12-02 17:32:04 +0700

    Update structure of project

---
+++ AimsProject/src/hust/soict/hedspi/javafx/JavaFXTest.java
@@ -0,0 +1,22 @@
+package hust.soict.hedspi.javafx;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class JavaFXTest extends Application {
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("Hello, JavaFX!");
+        Scene scene = new Scene(label, 300, 200);
+        stage.setScene(scene);
+        stage.setTitle("JavaFX App");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
